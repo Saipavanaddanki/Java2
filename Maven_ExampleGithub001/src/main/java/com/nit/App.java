aa74ee919c1500be7618604401e213aec1b978ea
sai <addanki.saipavan@gmail.com>
2024-02-27 20:08:28 +0530

    pl-committingo2

--- Maven_ExampleGithub001/src/main/java/com/nit/App.java
+++ Maven_ExampleGithub001/src/main/java/com/nit/App.java
@@ -2,7 +2,7 @@ package com.nit;
 
 /**
  * Hello world!
- *
+ *s
  */
 public class App 
 {
@@ -11,5 +11,17 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hii budies");
         System.out.println("---------------");
+        int n=50;
+        int m=55;
+        while(m!=n) {
+        	if(m>n) {
+        		m=m-n;
+        		
+        	}
+        	else {
+        		n=n-m;
+        	}
+        }
+        System.out.println("hcf of numbers: "+m);
     }
 }
