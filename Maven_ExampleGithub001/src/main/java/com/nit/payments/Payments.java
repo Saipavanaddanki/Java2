64c51a45ab02ebf2f7e79e9230f8d7aea309fa92
sai <addanki.saipavan@gmail.com>
2024-02-28 16:15:30 +0530

    projectleader-Branch2-payment02

--- Maven_ExampleGithub001/src/main/java/com/nit/payments/Payments.java
+++ Maven_ExampleGithub001/src/main/java/com/nit/payments/Payments.java
@@ -4,4 +4,7 @@ public class Payments {
 	public void upiPayments() {
 		System.out.println("Upi payments are done");
 	}
+	public void cardPayments() {
+		System.out.println("card payment are done");
+	}
 }
